89f2c3c5815142c04ece309abb71dc9c30b698db
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2020-11-11 17:21:15 -0300

    minor change + random time spent in bathroom

--- unisexBathroom/unisexBathroom.c
+++ unisexBathroom/unisexBathroom.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include<time.h> 
 
 // Problema de Unisex Bathroom, seção 6.2
 
@@ -34,18 +36,19 @@ void *man(void *arg) {
     // Se houver espaço no banheiro, pessoa ocupa
     if (bathroomSpots > 0) {
       printf("Há espaço no banheiro. Homem ocupando-o...\n");
-      peopleThatNeedToGoToTheBathroom--;
       bathroomSpots--;
       needsToGoToTheBathroom = 0;
     }
     pthread_mutex_unlock(&mutex);
   }
-  sleep(1); // tempo gasto dentro do banheiro
+  int randomTime = rand() % 1000001; 
+  usleep( randomTime ); // tempo aleátorio dentro do banheiro
 
   // Adquire o lock novamente para sair do banheiro
   pthread_mutex_lock(&mutex);
   printf("Homem saindo do banheiro\n");
   bathroomSpots++;
+  peopleThatNeedToGoToTheBathroom--; // Quando a pessoa sai do banheiro, marcamos isso para o contador
   // Ao sair do banheiro, uma vaga é liberada. Caso o banheiro fique livre, o último homem avisa as mulheres que não há mais homens no banheiro
   if (bathroomSpots == 3) {
     manSwitch = 0;
@@ -70,18 +73,19 @@ void *woman(void *arg) {
     manSwitch = 0;
     womanSwitch = 1;
     if (bathroomSpots > 0) {
-      printf("Há espaço no banheiro. Mulher ocupando-o e fazendo xixi...\n");
-      peopleThatNeedToGoToTheBathroom--;
+      printf("Há espaço no banheiro. Mulher ocupando-o...\n");
       bathroomSpots--;
       needsToGoToTheBathroom = 0;
     }
     pthread_mutex_unlock(&mutex);
   }
-  sleep(1); // tempo dentro do banheiro
+  int randomTime = rand() % 1000001; 
+  usleep( randomTime ); // tempo aleátorio dentro do banheiro
   pthread_mutex_lock(&mutex);
   printf("Mulher saindo do banheiro\n");
   bathroomSpots++;
-    // Ao sair do banheiro, uma vaga é liberada. Caso o banheiro fique livre, a última mulher avisa aos homens que não há mais mulheres no banheiro
+  peopleThatNeedToGoToTheBathroom--;
+  // Ao sair do banheiro, uma vaga é liberada. Caso o banheiro fique livre, a última mulher avisa aos homens que não há mais mulheres no banheiro
   if (bathroomSpots == 3) {
     manSwitch = 0;
     womanSwitch = 0;
@@ -92,18 +96,19 @@ void *woman(void *arg) {
 }
 
 int main(void) {
+  srand(time(NULL));
   pthread_t men[20];
   pthread_t women[20];
-  pthread_t toilet;
-  // Criando Thread para o Reitor
-  pthread_create(&toilet, NULL, bathroom, NULL);
+  pthread_t bathroomThread;
+  // Criando Thread para o banheiro
+  pthread_create(&bathroomThread, NULL, bathroom, NULL);
 
   for (int i = 0; i < 20; i++) {
     pthread_create(&men[i], NULL, man, NULL);
     pthread_create(&women[i], NULL, woman, NULL);
   }
 
-  // Programa só finaliza quando terminar a execução da thread do banheiro (todo mundo fez xixi)
-  pthread_join(toilet, NULL);
+  // Programa só finaliza quando terminar a execução da thread do banheiro (todo mundo foi)
+  pthread_join(bathroomThread, NULL);
   return 0;
 }
